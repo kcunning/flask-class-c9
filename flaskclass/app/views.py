b8858ae389f3e2f4ae56164ab87f1d19158e3d06
Katie Cunningham <katie.fulton@gmail.com>
2016-02-27 18:00:32 +0000

    Adding random

--- flaskclass/app/views.py
+++ flaskclass/app/views.py
@@ -1,11 +1,9 @@
+from random import randint
+
 from app import app
 from flask import render_template
  
 @app.route('/')
 def lucky_static():
-    lucky_num = 6
+    lucky_num = randint(1, 100)
     return render_template('simple.html', lucky_num=lucky_num)
-
-@app.route('/tpl/')
-def lucky_tpl():
-    return render_template('simple.html')
