b441299aea1041b71539329f996ccec55ecc655b
Katie Cunningham <katie.fulton@gmail.com>
2016-02-27 14:06:45 -0500

    Now the form works

--- flaskclass/app/views.py
+++ flaskclass/app/views.py
@@ -1,30 +1,41 @@
-from random import randint
-
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 from forms import GetLucky
- 
+
+from random import randint
+
 @app.route('/')
 def lucky_static():
-    lucky_num = randint(1, 100)
-    return render_template('simple.html', lucky_num=lucky_num)
-    
+	lucky_num = randint(1, 10)
+	return render_template('simple.html', lucky_num=lucky_num)
+
 @app.route('/<max>/')
 def lucky_max(max):
-    lucky_num = randint(1, int(max))
-    return render_template('simple.html', lucky_num=lucky_num)
+	lucky_num = randint(1, int(max))
+	return render_template('simple.html', lucky_num=lucky_num)
 
-@app.route('/game/')
-def game():
+def get_game_nums(num=5, max=10):
 	game_nums = []
-	while len(game_nums) < 5:
+	while len(game_nums) < num:
 		n = randint(1, 10)
 		if not n in game_nums:
 			game_nums.append(n)
- 	return render_template('game.html', game_nums = game_nums)
+	return game_nums
+
+
+@app.route('/game/')
+def game():
+	game_nums = get_game_nums()
+	return render_template('game.html', game_nums = game_nums)
 
 @app.route('/nums/', methods=['GET', 'POST'])
 def get_nums():
-    form = GetLucky()
-    return render_template('get_lucky.html', form=form)
+	form = GetLucky()
+	if request.method == 'POST':
+		numbers = form.numbers.data
+		game_nums = get_game_nums()	
+		return render_template('game.html', game_nums=game_nums,
+					player_nums=numbers)
+	if request.method == 'GET':	
+		return render_template('get_lucky.html', form=form)
