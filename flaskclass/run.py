b250bd1efed1ae850c9d163e7ff3ed652124626e
Katie Cunningham <katie.fulton@gmail.com>
2016-02-27 17:42:23 +0000

    Setting up the app properly.

--- flaskclass/run.py
+++ flaskclass/run.py
@@ -1,13 +1,9 @@
 import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'Hello World'
-
+from app import app
 
 # Host and port are unique to Cloud 9. You don't need these on your local,
 # or if using other services
-app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+app.run(
+    host=os.getenv('IP', '0.0.0.0'),
+    port=int(os.getenv('PORT', 8080)),
+    debug=True)
