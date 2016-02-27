84ec8a90ac867fa13903f6ebc21506564f80d6f8
Katie Cunningham <katie.fulton@gmail.com>
2016-02-27 17:54:09 +0000

    Let's add a variable.

--- flaskclass/app/views.py
+++ flaskclass/app/views.py
@@ -3,7 +3,8 @@ from flask import render_template
  
 @app.route('/')
 def lucky_static():
-    return "Your lucky number is 7"
+    lucky_num = 6
+    return render_template('simple.html', lucky_num=lucky_num)
 
 @app.route('/tpl/')
 def lucky_tpl():
